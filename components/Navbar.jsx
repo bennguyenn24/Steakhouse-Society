ef8cf92ec02b11339b81f7b4b5a3747d8ca9daef
Ben Nguyen <bennguyenn24@gmail.com>
2023-02-23 19:48:21 -0800

    Built out structure for navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+	return (
+		<nav>
+			{/* Logo */}
+			<section>
+				<h1>LOGO</h1>
+			</section>
+			{/* Links */}
+			<ul>
+				<li>Steaks</li>
+				<li>Meats</li>
+				<li>Chicken & Pork</li>
+				<li>Seafood</li>
+			</ul>
+			{/* Cart */}
+      <section>
+        <span>CART</span>
+      </section>
+		</nav>
+	);
+};
+
+export default Navbar;
