4d0fe1883c6796860511cbd8e9797aec606af9e4
Ben Nguyen <bennguyenn24@gmail.com>
2023-03-07 20:02:09 -0800

    Updated product for every product page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import "../styles/ProductCard.css"
 
 const ProductCard = ({ product }) => {
 	return (
-		<div className="product-card">
-			<img src={""} alt="" />
-			<h2>{product.name}</h2>
-			<p>{product.description}</p>
-			<div className="product-price">{product.price}</div>
-			<button>Add to cart</button>
+		<div className="wrapper">
+			<div className="product-img">
+				<img src={product.img} alt="" />
+			</div>
+			<div className="product-info">
+				<div className="product-text">
+					<h2>{product.name}</h2>
+					<p>{product.description}</p>
+				</div>
+				<div className="product-price-btn">
+					${product.price}
+					<button>Add to cart</button>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/data/products.js
+++
@@ -1,9 +0,0 @@
-const products = [
-	{ id: 1, img: "/", name: "Test", description: "Desc", price: 10 },
-    { id: 1, img: "/", name: "Test", description: "Desc", price: 10 },
-    { id: 1, img: "/", name: "Test", description: "Desc", price: 10 },
-    { id: 1, img: "/", name: "Test", description: "Desc", price: 10 },
-    { id: 1, img: "/", name: "Test", description: "Desc", price: 10 },
-];
-
-export default products;
